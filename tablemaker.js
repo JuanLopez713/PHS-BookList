15abcc0bf7dfe9dca19a864dca597fcf525afccb
JuanLopez713 <juan.lopez@providencehigh.org>
2017-04-17 01:12:21 -0700

    Cleaned Up
    Cleaned up unnecessary code

--- tablemaker.js
+++ tablemaker.js
@@ -2,56 +2,45 @@
 
 $(function () {
     var results = Papa.parse("BookList2016.csv", {
-        header: true
-        , download: true
-        , complete: function (results) {
+        header: true,
+        download: true,
+        complete: function (results) {
             console.log("Remote file parsed!", results.data);
             $.each(results.data, function (i, el) {
                 var row = $("<tr/>");
                 row.append($("<td/>").text());
                 $.each(el, function (j, cell) {
                     row.append($("<td/>").text(cell));
-                    
+
                 });
                 $("#books tbody").append(row);
-                
+
             });
             urlEncoder();
-//            var str = $('#books td:nth-child(4)').text();
-//            console.log(str);
-////            var str2 = 'https://www.google.com/#q=' + str;
-////            $('#books td:nth-child(4)').empty();
-//            $('#books td:nth-child(4)').prepend('<a href="https://www.w3schools.com">'+"test"+'</a>');
-            
-//            $('#books td:nth-child(4)').prepend('https://www.google.com/#q=');
-//            $('#books td:nth-child(4)').append("test");
-             
+
         }
-       
+
     });
-    
+
 });
 
 
-function urlEncoder(){
+function urlEncoder() {
     console.log("encoder called");
     var table = document.getElementById("books");
     var tr = table.getElementsByTagName("tr");
-    for (var i =0; i<tr.length; i++){
-       var td = tr[i].getElementsByTagName("td")[3];
-        
-        
-        if(td){
+    for (var i = 0; i < tr.length; i++) {
+        var td = tr[i].getElementsByTagName("td")[3];
+
+
+        if (td) {
             var str = td.innerHTML;
-        
-            td.innerHTML = '<a target="_blank" href="https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=' +str+'">'+str+'</a>';
-        
+
+            td.innerHTML = '<a target="_blank" href="https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=' + str + '">' + str + '</a>';
+
         }
-//    var str = $('#books td:nth-child(4)').text();
-//            var str2 = 'https://www.google.com/#q=' + str;
-//            $('#books td:nth-child(4)').empty();
-//            $('#books td:nh-child(4)').append(str2);
-}
+
+    }
 }
 
 
@@ -76,8 +65,7 @@ function searchTable() {
         }
         if (found) {
             tr[i].style.display = "";
-        }
-        else {
+        } else {
             tr[i].style.display = "none";
         }
         tr[0].style.display = "";
