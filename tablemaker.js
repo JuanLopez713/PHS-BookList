242c80de3486d314cf33dfd0363df5a4c9d20d48
JuanLopez713 <juan.lopez@providencehigh.org>
2017-04-17 01:11:04 -0700

    Amazon Links
    The ISBN numbers now properly link up to their respective amazon search.

--- tablemaker.js
+++ tablemaker.js
@@ -1,47 +1,67 @@
 //PARSE THE CRV
-$(function () {
 
+$(function () {
     var results = Papa.parse("BookList2016.csv", {
-        header: true,
-        download: true,
-        complete: function (results) {
+        header: true
+        , download: true
+        , complete: function (results) {
             console.log("Remote file parsed!", results.data);
             $.each(results.data, function (i, el) {
                 var row = $("<tr/>");
                 row.append($("<td/>").text());
                 $.each(el, function (j, cell) {
                     row.append($("<td/>").text(cell));
+                    
                 });
                 $("#books tbody").append(row);
+                
             });
-            var str = $('#books td:nth-child(4)').text();
-            str.prepend(("https://www.google.com/#q=").text());
-            $('#books td:nth-child(4)').append("test");
+            urlEncoder();
+//            var str = $('#books td:nth-child(4)').text();
+//            console.log(str);
+////            var str2 = 'https://www.google.com/#q=' + str;
+////            $('#books td:nth-child(4)').empty();
+//            $('#books td:nth-child(4)').prepend('<a href="https://www.w3schools.com">'+"test"+'</a>');
+            
+//            $('#books td:nth-child(4)').prepend('https://www.google.com/#q=');
+//            $('#books td:nth-child(4)').append("test");
+             
         }
+       
     });
-
-
-
-
-
-
-
-
-
+    
 });
 
 
+function urlEncoder(){
+    console.log("encoder called");
+    var table = document.getElementById("books");
+    var tr = table.getElementsByTagName("tr");
+    for (var i =0; i<tr.length; i++){
+       var td = tr[i].getElementsByTagName("td")[3];
+        
+        
+        if(td){
+            var str = td.innerHTML;
+        
+            td.innerHTML = '<a target="_blank" href="https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=' +str+'">'+str+'</a>';
+        
+        }
+//    var str = $('#books td:nth-child(4)').text();
+//            var str2 = 'https://www.google.com/#q=' + str;
+//            $('#books td:nth-child(4)').empty();
+//            $('#books td:nh-child(4)').append(str2);
+}
+}
+
+
 function searchTable() {
-
     var input, filter, table, i, x, y, terms, td, tr, found;
-
     input = document.getElementById("emp_search");
     filter = input.value.toUpperCase();
     terms = filter.split(" ");
     table = document.getElementById("books");
     tr = table.getElementsByTagName("tr");
-
-
     for (i = 0; i < tr.length; i++) {
         for (x = 0; x < 9; x++) {
             found = false;
@@ -51,17 +71,15 @@ function searchTable() {
                     if (td.innerHTML.toUpperCase().indexOf(terms[y]) > -1) {
                         found = true;
                     }
-
                 }
             }
-
         }
-        if(found){
-            tr[i].style.display="";
-        }else{
-            tr[i].style.display="none";
+        if (found) {
+            tr[i].style.display = "";
         }
-        tr[0].style.display="";
+        else {
+            tr[i].style.display = "none";
+        }
+        tr[0].style.display = "";
     }
-
 }
