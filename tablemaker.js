0b9a9e4746747887f9a379f3cf409eccc1985616
JuanLopez713 <juan.lopez@providencehigh.org>
2017-05-23 13:41:40 -0700

    Updates
    1. Updated Booklist to include course ids
    2. Changed search bar text
    3. Changed background color
    4. Allow searching only for course id
    5. PDF Button now fully functional

--- tablemaker.js
+++ tablemaker.js
@@ -1,7 +1,8 @@
 //PARSE THE CRV
 //(function(){
-//    alert("Make note of the course codes for future searches.");
+//    alert("Make note of the course ids found on your class schedules to use this tool.");
 //})();
+
 $(function () {
     var results = Papa.parse("BookList2017.csv", {
         header: true
@@ -35,6 +36,7 @@ function urlEncoder() {
 }
 var tr, td;
 var column, row;
+
 function searchTable() {
     var input, filter, table, i, x, y, terms, found;
     input = document.getElementById("emp_search");
@@ -45,8 +47,7 @@ function searchTable() {
     for (i = 0; i < tr.length; i++) {
         found = false;
         for (x = 0; x < 9; x++) {
-            td = tr[i].getElementsByTagName("td")[x];
-            
+            td = tr[i].getElementsByTagName("td")[8];
             if (td) {
                 for (y = 0; y < terms.length; y++) {
                     if (td.innerHTML.toUpperCase().indexOf(terms[y]) > -1) {
@@ -62,58 +63,72 @@ function searchTable() {
             tr[i].style.display = "none";
         }
         tr[0].style.display = "";
-       
     }
-    
 }
- var doc = new jsPDF('landscape');
-
+var doc = new jsPDF('landscape');
 $('#printbutt').click(function () {
-    var columns = ["Grade", "Department", "Course", "ISBN", "Author","Title","Publisher","Edition","Course Code","Notes"];
+    var columns = ["Grade", "Department", "Course", "ISBN", "Author", "Title", "Publisher", "Edition", "Course Code", "Notes"];
     var myBooks = [];
-    
     myBooks.push(columns);
-    
-   $("#books tr").each(function() {
-    var arrayOfThisRow = [];
-    var tableData = $(this).find('td');
-    if (tableData.length > 0) {
-        tableData.each(function() { arrayOfThisRow.push($(this).text()); });
-        myBooks.push(arrayOfThisRow);
-    }
-});
-    
-
-var rowsd=[];
-    
+    $("#books tr").each(function () {
+        var arrayOfThisRow = [];
+        var tableData = $(this).find('td');
+        if (tableData.length > 0) {
+            tableData.each(function () {
+                arrayOfThisRow.push($(this).text());
+            });
+            //            var isbn = arrayOfThisRow[3];
+            //           
+            //            arrayOfThisRow[3] =  doc.textWithLink(isbn, { url: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=' + isbn });
+            myBooks.push(arrayOfThisRow);
+        }
+    });
+    var rowsd = [];
     var input = document.getElementById("emp_search");
     filter = input.value;
-    
-    if(!filter){
+    if (!filter) {
         alert("Please Type your Course Codes into the Search Bar");
-    }else{
-    
-    for (var i=1; i<tr.length; i++){
-        
-        if(tr[i].style.display==""){
-            rowsd.push(myBooks[i]);
-            console.log("concatenating: " + tr[i]);
-        }else{
-            
+    }
+    else {
+        for (var i = 1; i < tr.length; i++) {
+            if (tr[i].style.display == "") {
+                rowsd.push(myBooks[i]);
+                console.log("concatenating: " + tr[i]);
+            }
+            else {}
         }
-        
+        //        for (var x = 0; x < rowsd.length; x++) {
+        //            var isbn = rowsd[x][3].text;
+        //            console.log(isbn);
+        //            rowsd[x][3] = 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=' + rowsd[x][3];
+        //        }
+        console.log("This is the rows: " + rowsd);
+        var links = [];
+        doc.autoTable(myBooks[0], rowsd, {
+            theme: 'striped'
+            , styles: {
+                overflow: 'linebreak'
+                , columnWidth: 27
+            }
+            , drawCell: function (cell, opts) {
+                if (opts.column.dataKey === 5) {
+                    links.push({
+                        x: cell.textPos.x+10
+                        , y: cell.textPos.y
+                    });
+                }
+            }
+            , addPageContent: function () {
+                for (var i = 0; i < links.length; i++) {
+                    var isbn = rowsd[i][3];
+                    delete rowsd[i][3];
+                    doc.textWithLink('', links[i].x, links[i].y+10, {
+                        url: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=' + isbn
+                    });
+                    
+                }
+            }
+        });
+        doc.save('table.pdf');
     }
-    console.log("This is the rows: " + rowsd);
-       
-doc.autoTable(myBooks[0], rowsd, {
-    theme: 'striped',
-    styles: {
-        
-      overflow: 'linebreak',
-      
-      columnWidth: 27
-    }
-  });
-doc.save('table.pdf');
-}});
-
+});
