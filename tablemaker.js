ef1d45c173f511c0e6ac9f3b729e36da68aadecc
JuanLopez713 <juan.lopez@providencehigh.org>
2017-06-15 13:45:52 -0700

    testing 2.0

--- tablemaker.js
+++ tablemaker.js
@@ -4,7 +4,7 @@
 //    alert("Make note of the course ids found on your class schedules to use this tool.");
 //})();
 $(function () {
-    var results = Papa.parse("BookList2017.csv", {
+    var results = Papa.parse("BookList2017.csv?Version=1.4", {
         header: true
         , download: true
         , complete: function (results) {
